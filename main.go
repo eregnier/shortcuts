55fe587e92b512b7ff907802212ca5d5d63f8327
Eric Regnier <eric.regnier.mail@gmail.com>
2022-06-16 12:06:51 +0200

    feat: handle task hierarchy

--- main.go
+++ main.go
@@ -6,69 +6,65 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/webview/webview"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	"strings"
+	"strconv"
 )
 
 type Action struct {
-	Name    string `json:"name"`
-	Command string `json:"command,omitempty"`
+	Name     string   `json:"name"`
+	Command  string   `json:"command"`
+	Commands []Action `json:"commands"`
 }
 
 //go:embed index.html
 var index []byte
 
 var commands []Action
-var DEV bool
 
 func main() {
-
-	DEV = os.Getenv("DEV") == "1"
 
 	commands = loadCommands()
 	r := gin.Default()
 	r.GET("/actions", handleListActions)
 	r.GET("/", handleHome)
 
-	r.GET("/task", handleTask)
+	r.GET("/trigger", handleTask)
 
-	if DEV {
-		err := r.Run(":8144")
-		if err != nil {
-			log.Fatalln("could not start app")
-		}
-
-	} else {
-		go func() {
-			err := r.Run(":8144")
-			if err != nil {
-				log.Fatalln("could not start app")
-			}
-		}()
-		w := webview.New(true)
-		defer w.Destroy()
-		w.SetTitle("plip")
-		w.SetSize(800, 600, webview.HintNone)
-		w.Navigate("http://localhost:8144")
-		w.Run()
+	err := r.Run(":8144")
+	if err != nil {
+		log.Fatalln("could not start app")
 	}
+
 }
 
 func handleTask(c *gin.Context) {
 	action := c.Query("action")
+	position := c.Query("position")
 	if action == "quit" {
 		c.JSON(200, gin.H{})
 		os.Exit(0)
 	}
-	for _, item := range commands {
+
+	var selectedCommands []Action
+	if position != "undefined" {
+		x, err := strconv.Atoi(position)
+		if err != nil {
+			c.JSON(400, gin.H{"message": "wrong item position selection"})
+			return
+		}
+		selectedCommands = commands[x].Commands
+	} else {
+		selectedCommands = commands
+	}
+
+	for _, item := range selectedCommands {
 		if item.Name == action {
-			args := strings.Fields(item.Command)
-			cmd := exec.Command(args[0], args[1:]...)
+			fmt.Println("do command", "./commands/"+item.Command)
+			cmd := exec.Command("./commands/" + item.Command)
 			var out bytes.Buffer
 			cmd.Stdout = &out
 			err := cmd.Run()
@@ -79,9 +75,6 @@ func handleTask(c *gin.Context) {
 		}
 	}
 	c.JSON(200, gin.H{})
-	if !DEV {
-		os.Exit(0)
-	}
 }
 
 func handleHome(c *gin.Context) {
